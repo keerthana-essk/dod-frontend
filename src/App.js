7c4501f37ef7d666f22677f198024a2c517d6f19
leo <leo@leo.leo>
2024-07-28 04:08:32 -0500

    first commitoilio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./screens/Home/Home";
 import Edit from "./screens/Edit/Edit";
 import Start from "./screens/Start/Start";
